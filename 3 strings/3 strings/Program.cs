93e059402507113eebe9ab6d6c1f2bf2f9015fa8
Melissa DoddRuesing <melissa.doddruesing@gmail.com>
2018-07-27 15:52:54 -0500

    committ all

---
+++ 3 strings/3 strings/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _3_strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("who are you?");
+            Console.Write("type your first name: ");
+            string myFirstName = Console.ReadLine();
+           
+            Console.Write("type your last name: ");
+            string myLastName = Console.ReadLine();
+
+            Console.WriteLine("type your gender: ")
+            string myGender = Console.ReadLine
+
+            Console.WriteLine("myFirstName" + "myLastName" + "myGender");
+            Console.ReadLine();
+
+            //3 strings, simple questions
+
+
+                    }
+    }
+}
---
+++ rectangle/rectangle/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace rectangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //asking user for dimensions with Kendras help
+            Console.WriteLine("what is height?");
+            string height = Console.ReadLine();
+            int number = int.Parse("height")
+            
+      
+            Console.WriteLine("what is width");
+            string width = Console.ReadLine();
+            NewMethod();
+            Console.WriteLine(number);
+
+            Console.WriteLine("what is height" + "what is width");
+            Console.ReadLine();
+
+        }
+
+        private static void NewMethod()
+        {
+            int number = int.Parse("width");
+        }
+    }
+}
